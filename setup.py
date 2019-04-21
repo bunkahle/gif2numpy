d5d0d27ad4e9d467343a7716b21b2aa433713ed4
bunkahle <bunkahle@users.noreply.github.com>
2019-04-21 15:02:46 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     #   3 - Alpha
     #   4 - Beta
     #   5 - Production/Stable
-    'Development Status ::  5 - Production/Stable',
+    'Development Status :: 5 - Production/Stable',
     'License :: OSI Approved :: MIT License',
     # Specify the Python versions you support here. In particular, ensure
     # that you indicate whether you support Python 2, Python 3 or both.
